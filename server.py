5d9262bc79c329fe97ec4d76dc2803a50cd51a38
Loretta Jimenez <loretta_94111@yaho.com>
2021-10-09 00:14:28 +0000

    Created base.html and type.html

--- server.py
+++ server.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/type')
+def browse_by_type():
+    return render_template('type.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
