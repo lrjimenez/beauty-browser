b468c29d0e441cd77cb1868b6f9cc10d96d3cd3b
Loretta Jimenez <loretta_94111@yaho.com>
2021-11-04 01:01:22 +0000

    Final Styling

--- static/js/chart.js
+++ static/js/chart.js
@@ -3,15 +3,22 @@ console.log($('#bar-chart'))
 new Chart($('#bar-chart'), {
   type: 'bar',
   data: {
-    labels: [
-    "orly",
-    "suncoat",
-    "marcelle", 
-   ],
+      labels: [
+        "orly",
+        "suncoat",
+        "marcelle", 
+        ],
+  
+     
     datasets: [
       {
         label: "Average Product Rating",
         data: [3.6, 3.76, 4.25],
+        backgroundColor: [
+          "#F4F7C1",
+          "#C1F7F4",
+          "#E4CDF7",
+        ],
       },
     ],
   }
