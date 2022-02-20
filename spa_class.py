eb89f89a1506debe9f7893e0b828340e6538a3a3
Loretta Jimenez <lorettajimenez@Lorettas-MacBook-Pro.local>
2022-02-20 13:41:01 -0800

    Adding spa class

---
+++ spa_class.py
@@ -0,0 +1,11 @@
+"""A class of Spas"""
+class Spa: 
+    def __init__(self, name, address, website, services, products, ratings, social):
+        self.name = name
+        self.address = address
+        self.website = website
+        self.services = services
+        self.products = products
+        self.ratings = ratings
+        self.social = social
+        
